5579ada1ea61f15ed9ed4e24e79b89fc867e984d
Rick214 <rickvoermans07@gmail.com>
2025-06-18 15:19:43 +0200

    HolidayFilter

--- trashtracker1/Components/Code/LitterFilters.cs
+++ trashtracker1/Components/Code/LitterFilters.cs
@@ -4,6 +4,11 @@ using System.Linq;
 
 namespace trashtracker1.Components.Code
 {
+    public class Holiday
+    {
+        public string Date { get; set; }
+        public string LocalName { get; set; }
+    }
     public class LitterFilters
     {
         public Random rnd = new Random();
@@ -11,9 +16,9 @@ namespace trashtracker1.Components.Code
         public int LitterAmount;
         public int filteredLitterAmount;
         public List<int> litterData = new List<int>();
-        public List<string> litterDays = new List<string>();
+        public List<string> litterDays = new List<string> { "18-6", "17-6", "16-6", "15-6", "14-6", "13-6", "12-6" };
 
-        public string[] Labels = ["Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo"];
+        public string[] Labels = [];
         public int[] Values;
         public string ChartTitle = "Afval per dag";
 
@@ -56,15 +61,36 @@ namespace trashtracker1.Components.Code
         {
             litterData.Clear();
             litterDays.Clear();
-            for (int i = days; i > 0; i--)
+            for (int i = (days - 1); i >= 0; i--)
             {
                 litterData.Add(rnd.Next(0, 100));
                 litterDays.Add(DateTime.Now.AddDays(-i).ToString("dd-MM"));
             }
             Values = litterData.ToArray();
             Labels = litterDays.ToArray();
+            await GetHolidayData();
             await UpdateChartAsync();
         }
-
+        public List<Holiday> displayedHolidays = new();
+        private List<Holiday> holidays = new()
+        {
+        new Holiday { Date = new DateTime(2025, 6, 18).ToString("dd-MM"), LocalName = "Nieuwjaarsdag" },
+        new Holiday { Date = new DateTime(2025, 6, 17).ToString("dd-MM"), LocalName = "Goede Vrijdag" },
+        new Holiday { Date = new DateTime(2025, 6, 16).ToString("dd-MM"), LocalName = "Eerste Paasdag" }
+        };
+        public async Task GetHolidayData()
+        {
+            displayedHolidays.Clear();
+            foreach (string day in litterDays)
+            {
+                foreach (Holiday holiday in holidays)
+                {
+                    if (holiday.Date.Contains(day))
+                    {
+                        displayedHolidays.Add(holiday);
+                    }
+                }
+            }
+        }
     }
 }

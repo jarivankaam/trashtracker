2211f92a4749014784e401aac19b48d94bda4d4d
mikeyboy52 <mikevanlommel2007@gmail.com>
2025-06-23 20:33:07 +0200

    fixed AdminPanel And UserCreateDto

---
+++ trashtracker1/Components/HelperServices/API/Dto/UserCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace trashtracker1.Components.HelperServices.API.Dto
+{
+    public class UserCreateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string AdminRole { get; set; }
+    }
+}

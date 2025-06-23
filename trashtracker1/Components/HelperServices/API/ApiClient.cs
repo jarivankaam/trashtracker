250f4b87ce286b555e7f6944dc2e6525e3d5eb86
jarivankaam <jari@jarivankaam.nl>
2025-06-23 21:38:23 +0200

    add token and login function

--- trashtracker1/Components/HelperServices/API/ApiClient.cs
+++ trashtracker1/Components/HelperServices/API/ApiClient.cs
@@ -1,14 +1,31 @@
-﻿namespace trashtracker1.Components.HelperServices.API
+﻿using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+
+namespace trashtracker1.Components.HelperServices.API
 {
     public class ApiClient
     {
         private readonly HttpClient _httpClient;
-        public ApiClient(HttpClient httpClient)
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ApiClient(HttpClient httpClient, IHttpContextAccessor contextAccessor)
         {
             _httpClient = httpClient;
+            _contextAccessor = contextAccessor;
         }
+
+        private void AttachJwtHeader()
+        {
+            var token = _contextAccessor.HttpContext?.Request.Cookies["authToken"];
+            if (!string.IsNullOrEmpty(token))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+
         public async Task<List<Dto.FavoriteLocationDto>> GetFavoriteLocationAsync()
         {
+            AttachJwtHeader();
             var response = await _httpClient.GetAsync("https://avansict2231011.azurewebsites.net");
             if (response.IsSuccessStatusCode)
             {
@@ -20,8 +37,10 @@
                 return new List<Dto.FavoriteLocationDto>();
             }
         }
+
         public async Task<List<Dto.HolidaysDto>> GetHolidaysAsync()
         {
+            AttachJwtHeader();
             var response = await _httpClient.GetAsync("https://avansict2231011.azurewebsites.net/api/External/holidays");
             if (response.IsSuccessStatusCode)
             {
@@ -33,8 +52,10 @@
                 return new List<Dto.HolidaysDto>();
             }
         }
+
         public async Task<List<Dto.LitterDto>> GetLitterAsync()
         {
+            AttachJwtHeader();
             var response = await _httpClient.GetAsync("https://avansict2231011.azurewebsites.net/api/Litter/GetAllLitter");
             if (response.IsSuccessStatusCode)
             {
@@ -46,8 +67,10 @@
                 return new List<Dto.LitterDto>();
             }
         }
+
         public async Task<List<Dto.PredictionDto>> GetPredictionAsync()
         {
+            AttachJwtHeader();
             DateTime date = DateTime.Now.AddDays(1);
             string currentdate = date.ToString("yyyy-MM-dd");
             DateTime nextMonth = DateTime.Now.AddDays(1).AddMonths(1);
@@ -64,8 +87,10 @@
                 return new List<Dto.PredictionDto>();
             }
         }
+
         public async Task<List<Dto.UserDto>> GetUserAsync()
         {
+            AttachJwtHeader();
             var response = await _httpClient.GetAsync("https://avansict2231011.azurewebsites.net");
             string statusCode = Convert.ToString(response.StatusCode);
             if (response.IsSuccessStatusCode)
@@ -78,6 +103,5 @@
                 return new List<Dto.UserDto>();
             }
         }
-
     }
 }
